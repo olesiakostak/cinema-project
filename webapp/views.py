4d8b7a3f98b60129e9e9b28177beea45c9d919ef
Olesia <Olesiakostak@gmail.com>
2025-09-10 13:15:25 +0300

    lab5

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,4 @@
-from django.views.generic import ListView
-from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView
 from app.catalog.repositories import unit_of_work
 
 
