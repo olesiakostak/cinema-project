31f814369887d98e25b861eb10d12937224881d8
Olesia <Olesiakostak@gmail.com>
2025-09-10 19:02:04 +0300

    lab5

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import GenreListView, GenreDetailView
+from . import views
 
 app_name = 'webapp'
 
 urlpatterns = [
-    path('genres/', GenreListView.as_view(), name='genre-list'),
-    path('genres/<int:pk>/', GenreDetailView.as_view(), name='genre-detail')
+    path('genres/', views.GenreListView.as_view(), name='genre-list'),
+    path('genres/<int:pk>/', views.GenreDetailView.as_view(), name='genre-detail'),
+    path('genres/add/', views.GenreCreateView.as_view(), name='genre-add'),
+    path('genres/update/<int:pk>/', views.GenreUpdateView.as_view(), name='genre-update'),
+    path('genres/delete/<int:pk>/', views.GenreDeleteView.as_view(), name='genre-delete'),
+    path('films/', views.FilmListView.as_view(), name='film-list'),
+    path('films/<int:pk>/', views.FilmDetailView.as_view(), name='film-detail')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from app.catalog.repositories import unit_of_work
+from django.urls import reverse_lazy
+from app.catalog.models import Genre, Film
 
 
 class GenreListView(ListView):
@@ -8,10 +10,42 @@ class GenreListView(ListView):
 
     def get_queryset(self):
         return unit_of_work.genres.get_all()
-    
+
 class GenreDetailView(DetailView):
     template_name = 'webapp/genre_detail.html'
     context_object_name = 'genre'
 
     def get_queryset(self):
         return unit_of_work.genres.get_all()
+    
+class GenreCreateView(CreateView):
+    model = Genre
+    fields = ['name']
+    template_name = 'webapp/genre_form.html'
+    success_url = reverse_lazy('webapp:genre-list')
+
+class GenreUpdateView(UpdateView):
+    model = Genre
+    fields = ['name']
+    template_name = 'webapp/genre_form.html'
+    success_url = reverse_lazy('webapp:genre-list')
+
+class GenreDeleteView(DeleteView):
+    model = Genre
+    template_name = 'webapp/genre_confirm_delete.html'
+    success_url = reverse_lazy('webapp:genre-list')
+
+
+class FilmListView(ListView):
+    template_name = 'webapp/film_list.html'
+    context_object_name = 'films'
+
+    def get_queryset(self):
+        return unit_of_work.films.get_all()
+    
+class FilmDetailView(DetailView):
+    template_name = 'webapp/film_detail.html'
+    context_object_name = 'film'
+
+    def get_queryset(self):
+        return unit_of_work.films.get_all()
