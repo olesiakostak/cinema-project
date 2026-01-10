2d34353e8b8521dfd9fcd69e3e0517e5d391b74e
Olesia <Olesiakostak@gmail.com>
2026-01-10 16:27:13 +0200

    adding language

--- cinema_project/urls.py
+++ cinema_project/urls.py
@@ -18,9 +18,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("app.catalog.urls")),
     path("", include("webapp.urls"))
 ]
+
+urlpatterns += i18n_patterns(
+    path("admin/", admin.site.urls),
+    path("api/", include("app.catalog.urls")),
+    path("", include("webapp.urls"))
+)
